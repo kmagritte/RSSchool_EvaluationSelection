bbbe23f512a2fe40a8718c2f5ffecd6c8210f9d3
kmagritte <k.kshr@yandex.ru>
2022-04-30 15:25:14 +0300

    Add option random-state in train

--- src/forest_ml/train.py
+++ src/forest_ml/train.py
@@ -11,9 +11,17 @@ from .data import get_dataset
     default="data/train.csv",  
     type=click.Path(exists=True, dir_okay=False, path_type=Path)
 )
+@click.option(
+    "--random-state",
+    default=42,
+    type=int,
+    show_default=True,
+)
 def train(
-    dataset_path: Path
+    dataset_path: Path,
+    random_state: int,
 ) -> None:
     features_train, features_val, target_train, target_val = get_dataset(
         dataset_path,
+        random_state,
     )
