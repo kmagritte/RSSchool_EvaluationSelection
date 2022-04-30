89c81e727dc7bae47ade75940519f40594ec2877
kmagritte <k.kshr@yandex.ru>
2022-04-30 15:29:17 +0300

    Add option test-split-ratio in train

--- src/forest_ml/data.py
+++ src/forest_ml/data.py
@@ -5,13 +5,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def get_dataset(
-    csv_path: Path, random_state: int#, test_split_ratio: float
+    csv_path: Path, random_state: int, test_split_ratio: float
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     dataset = pd.read_csv(csv_path, index_col='Id')
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop("Cover_Type", axis=1)
     target = dataset["Cover_Type"]
     features_train, features_val, target_train, target_val = train_test_split(
-        features, target, random_state=random_state,# test_size=test_split_ratio
+        features, target, random_state=random_state, test_size=test_split_ratio
     )
     return features_train, features_val, target_train, target_val
--- src/forest_ml/train.py
+++ src/forest_ml/train.py
@@ -17,11 +17,19 @@ from .data import get_dataset
     type=int,
     show_default=True,
 )
+@click.option(
+    "--test-split-ratio",
+    default=0.2,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+    show_default=True,
+)
 def train(
     dataset_path: Path,
     random_state: int,
+    test_split_ratio: float,
 ) -> None:
     features_train, features_val, target_train, target_val = get_dataset(
         dataset_path,
         random_state,
+        test_split_ratio,
     )
